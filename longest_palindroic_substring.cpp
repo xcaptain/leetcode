71f8c9e2167aac12cdb9ccd7f7d8afd2f45f2bf1
Joey <joey.xf@gmail.com>
2018-07-17 11:10:11 +0800

    使用动态规划的方法，保存计算中间值，避免重复计算
    这个解法已经被leetcode AC

--- longest_palindroic_substring.cpp
+++ longest_palindroic_substring.cpp
@@ -1,41 +1,75 @@
 /**
  * 问题链接：https://leetcode.com/problems/longest-palindromic-substring/description/
  * 编译命令：clang++ longest_palindroic_substring.cpp -std=c++14
- * 回答状态：Time Limit Exceeded
+ * 回答状态：AC
  */
 #include <iostream>
 #include <string>
+#include <vector>
 #include <assert.h>
 #include <algorithm>
 using namespace std;
 
-// TODO: fixme
 class Solution
 {
   public:
     std::string longestPalindrome(std::string s)
     {
         int l = s.length();
-        std::string result = s.substr(0, 1);
-        int max_len = 1;
+        int boundTable[l][l]; // s[i..j]如果是回文串，则值是串的长度
+        std::string sub_s;
+
+        for (int i = 0; i < l; i++)
+        {
+            for (int j = 0; j < l; j++)
+            {
+                boundTable[i][j] = 0;
+            }
+        }
+
+        // 长度为1的子串都是回文串
+        int maxLen = 1;
+        for (int i = 0; i < l; i++)
+        {
+            boundTable[i][i] = 1;
+            sub_s = s.substr(i, 1);
+            // std::cout << "sub_s: " << sub_s << std::endl;
+        }
+
+        // 查找长度为2的回文串
+        int start = 0;
         for (int i = 0; i < l - 1; i++)
         {
-            for (int j = i + 1; j < l; j++)
+            if (s[i] == s[i + 1])
             {
-                std::string sub_s = s.substr(i, j - i + 1);
-                // std::cout << "substring: " << sub_s << std::endl;
-                if (this->isPalindrome(sub_s))
+                boundTable[i][i + 1] = 2;
+                maxLen = 2;
+                start = i;
+                sub_s = s.substr(i, 2);
+                // std::cout << "sub_s: " << sub_s << std::endl;
+            }
+        }
+
+        // 查找长度为3以上的回文串
+        for (int k = 3; k <= l; k++)
+        {
+            for (int i = 0; i < l - k + 1; i++)
+            {
+                int j = i + k - 1;
+                // 如果i+1到j-1已经是回文串，而s[i] == s[j]，则i..j也是回文串
+                if (boundTable[i+1][j-1] != 0 && s[i] == s[j])
                 {
-                    int sub_l = j - i + 1;
-                    if (sub_l > max_len)
+                    boundTable[i][j] = k;
+                    if (k > maxLen)
                     {
-                        max_len = sub_l;
-                        result = sub_s;
+                        maxLen = k;
+                        sub_s = s.substr(i, k);
                     }
                 }
             }
         }
-        return result;
+
+        return sub_s;
     }
 
   private:
@@ -43,8 +77,10 @@ class Solution
     {
         int start = 0;
         int end = s.length() - 1;
-        while (start < end) {
-            if (s[start] != s[end]) {
+        while (start < end)
+        {
+            if (s[start] != s[end])
+            {
                 return false;
             }
             start++;
@@ -58,7 +94,7 @@ class Solution
 int main(int argc, char const *argv[])
 {
     Solution s;
-    std::string s1 = "banana";
+    std::string s1 = "bananaa";
     std::string s2 = "a";
     // assert(s.longestPalindrome(s1) == ps1);
     std::cout << s.longestPalindrome(s1) << std::endl;
